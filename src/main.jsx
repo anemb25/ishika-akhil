5cfa0e16110bdca0530feae6dc6610cbb8ab48eb
anemb25 <anem.b25@fms.edu>
2026-02-09 01:43:16 +0530

    Switch to HashRouter to fix Netlify routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Intro from "./Intro.jsx";     // 👈 NEW
 import App from "./App.jsx";         // game
@@ -11,17 +12,11 @@ import "./proposal.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* INTRO PAGE (FIRST SCREEN) */}
-        <Route path="/" element={<Intro />} />
-
-        {/* GAME */}
-        <Route path="/game" element={<App />} />
-
-        {/* PROPOSAL */}
-        <Route path="/proposal" element={<Proposal />} />
-      </Routes>
-    </BrowserRouter>
+ <HashRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/proposal" element={<Proposal />} />
+  </Routes>
+</HashRouter>
   </React.StrictMode>
 );
