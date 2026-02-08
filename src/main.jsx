3a25f81c216e240c86ead3ec60fd8046705c24da
anemb25 <anem.b25@fms.edu>
2026-02-09 01:48:25 +0530

    Fix router imports and routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Intro from "./Intro.jsx";     // 👈 NEW
+import Intro from "./intro.jsx";     // landing / hook screen
 import App from "./App.jsx";         // game
-import Proposal from "./Proposal.jsx";
+import Proposal from "./proposal.jsx";
 
 import "./styles.css";
 import "./proposal.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- <HashRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/proposal" element={<Proposal />} />
-  </Routes>
-</HashRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/game" element={<App />} />
+        <Route path="/proposal" element={<Proposal />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
